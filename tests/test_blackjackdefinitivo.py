f688957880702545d444a362d7b9cdabf278b854
ricitos2001 <cgucha@iesrafaelalberti.ess>
2023-11-08 13:45:02 +0100

    version definitiva del blackjack

--- tests/test_blackjackdefinitivo.py
+++ tests/test_blackjackdefinitivo.py
@@ -1,12 +1,22 @@
 import pytest
-from src.menu import iniciar_modo
-from src.blackjackcompleto import crear_resultadosolitario
-from src.blackjackcompleto import crear_resultadomultijugador
+from src.blackjackdefinitivo import iniciar_modo
+from src.blackjackdefinitivo import crear_resultadosolitario
+from src.blackjackdefinitivo import crear_resultadomultijugador
 @pytest.mark.parametrize(
     "modo,mododejuego",
     [
         (1,"iniciando modo solitario"),
         (2,"iniciando modo multijugador")
+    ]
+)
+def test_iniciar_modo_params(modo,mododejuego):
+    assert iniciar_modo(modo)==mododejuego
+
+@pytest.mark.parametrize(
+    "modo,mododejuego",
+    [
+        (1,"iniciando modo solitario\n"),
+        (2,"iniciando modo multijugador\n")
     ]
 )
 def test_iniciar_modo_params(modo,mododejuego):
@@ -29,7 +39,7 @@ def test_crear_resultado_params(manojugador,manodistribuidor,resultadosolitario)
 @pytest.mark.parametrize(
     "manojugador,manodistribuidor,resultadomultijugador",
     [
-        (['K', 'A'],['Q', 9],"\ntu tienes ['K', 'A'] para un total de 21 puntos y el distribuidor tiene ['z', 9] para un total de 19 puntos\nBlackjack: jugador gana!"),
+        (['K', 'A'],['Q', 9],"\ntu tienes ['K', 'A'] para un total de 21 puntos y el distribuidor tiene ['Q', 9] para un total de 19 puntos\nBlackjack: jugador gana!"),
         (['J', 6],[4, 8, 9],"\ntu tienes ['J', 6] para un total de 16 puntos y el distribuidor tiene [4, 8, 9] para un total de 21 puntos\nBlackjack: distribuidor gana!"),
         (['K', 5],['Q', 'J', 'J'],"\ntu tienes ['K', 5] para un total de 15 puntos y el distribuidor tiene ['Q', 'J', 'J'] para un total de 30 puntos\ndistribuidor revienta! jugador gana!"),
         (['A', 'A'],[4, 9],"\ntu tienes ['A', 'A'] para un total de 22 puntos y el distribuidor tiene [4, 9] para un total de 13 puntos\njugador revienta! distribuidor gana!"),
